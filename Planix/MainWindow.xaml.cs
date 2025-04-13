b64a3d93829c05add44d152716e5bceb4f85ebf5
hoaihaii-el <hoaihai.el@gmail.com>
2025-04-13 12:08:37 +0700

    feat: create UI structure; add styles; custom calendar control

---
+++ Planix/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Planix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PowerOff_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
