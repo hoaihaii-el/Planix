b5f5020087b19774650b211d32b13cca8fe322ae
hoaihaii-el <hoaihai.el@gmail.com>
2025-04-19 01:37:36 +0700

    feat: custom control PlaceholderTextBox base on TextBox

---
+++ CustomControls/PlaceholderTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControls
+{
+    public class PlaceholderTextBox : TextBox
+    {
+        static PlaceholderTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(PlaceholderTextBox), 
+                new FrameworkPropertyMetadata(typeof(PlaceholderTextBox))
+            );
+        }
+
+        // Dependency Property cho PlaceholderText
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.Register(
+                nameof(PlaceholderText), 
+                typeof(string), 
+                typeof(PlaceholderTextBox),
+                new PropertyMetadata("Enter text here...")
+            );
+
+        public string PlaceholderText
+        {
+            get => (string)GetValue(PlaceholderTextProperty);
+            set => SetValue(PlaceholderTextProperty, value);
+        }
+    }
+}
